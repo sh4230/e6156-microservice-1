a81052bbb499437717f4776c46e8a19563031e39
Sophia Horng <sh4230@columbia.edu>
2023-09-20 13:22:38 -0400

    update

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/sophiahorng/Desktop/Columbia/coms6156-cloudcomputing/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
